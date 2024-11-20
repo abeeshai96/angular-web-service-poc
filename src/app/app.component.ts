2c4d119ef0b8e8cb25b94e3e8e1c6f19553fe7c1
Abeesh A I <abeeshai96@gmail.com>
2024-11-20 17:33:51 +0530

    Init Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,46 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Observable } from 'rxjs';
+import { XMLParser } from 'fast-xml-parser';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, HttpClientModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.less'
+})
+export class AppComponent {
+  title = 'angular-web-service';
+  private url = '/api/csp/samples/SOAP.Demo.cls?soap_method=AddInteger&Arg1=1&Arg2=1';
+  xmlData: any;
+  jsonData: any;
+
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.fetchData();
+  }
+
+  getData(): Observable<any> {
+    return this.http.get(`${this.url}`, { responseType: 'text' })
+  }
+
+  fetchData() {
+    this.getData().subscribe({
+      next: (response) => {
+        this.xmlData = response;
+        this.jsonData = JSON.stringify(this.convertXmlToJson(response));
+      },
+      error: (error) => {
+        console.error(error);
+      },
+    });
+  }
+
+  convertXmlToJson(xml: string): any {
+    const parser = new XMLParser();
+    return parser.parse(xml);
+  }
+}
